a160c4b71bc1b8dc71e6633e50b971a3bdff5b58
surfgg <gkoynash1809@gmail.com>
2024-03-14 11:47:54 +0700

    first program but no checkout if 10-2-3

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func input() {
+	var num1, num2, operator string
+
+	fmt.Println("Введите цифры от 0 до 10 и выполняемое действие '+' '-' '%' '*' через пробел:")
+	count, err := fmt.Scanf("%s %s %s", &num1, &operator, &num2)
+
+	if err != nil {
+		fmt.Printf("Ошибка при чтении ввода, выполните условие ввода: %v\n", err)
+	} else if count != 3 {
+		fmt.Printf("Ошибка при чтении ввода, выполните условие ввода")
+	} else {
+		fmt.Printf("Вы ввели %s %s %s\n", num1, operator, num2)
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello")
+	input()
 }
